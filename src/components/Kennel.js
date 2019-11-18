ac6dba98324850e1caab6e90baf1cc0b2c29a30c
John Long <jlcomp45@gmail.com>
2019-11-18 15:18:28 -0600

    completed chapter 1 and chapter 2 - next chapters will be branches

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,38 +1,18 @@
-import React, { Component } from 'react'
-import AnimalCard from './animal/AnimalCard'
-import './Kennel.css'
-import LocationCard from './location/LocationCard'
-import OwnerCard from "./owner/OwnerCard"
-import EmployeeCard from './employee/EmployeeCard'
+import React, { Component } from "react"
+import NavBar from "./nav/NavBar"
+import ApplicationViews from "./ApplicationViews"
+
+import "./Kennel.css"
 
 class Kennel extends Component {
-    render() {
-        return (
-            <div>
-                <div>
-                    <h2>Student Kennels<br />
-                        <small>Loving care when you're not there.</small>
-                    </h2>
-                    <address>
-                        Visit Us at the Nashville North Location
-            <br />500 Puppy Way
-          </address>
-                </div>
-                <div>
-                    <AnimalCard />
-                </div>
-                <div>
-                    <LocationCard />
-                </div>
-                <div>
-                    <OwnerCard />
-                </div>
-                <div>
-                    <EmployeeCard />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <ApplicationViews />
+      </React.Fragment>
+    )
+  }
 }
 
-export default Kennel;
+export default Kennel
